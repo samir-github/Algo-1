c936f067c2f4318baa4836e06b1a5950b2402695
samir-github <Samir>
2015-06-20 17:41:08 +0300

    Updating

--- week1/1-Vector/vector.cpp
+++ week1/1-Vector/vector.cpp
@@ -1,46 +1,163 @@
+#include <iostream>
+#include <typeinfo>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
 template<class T>
 class Vector {
+
+private:
+
+    T m_capacity = 1;
+    T m_size = 0;
+    T* arr = new int[m_capacity];
+
 public:
 
-  // Adds value at a specific index in the Vector.
-  // Complexity: O(n)
-  void insert(int index, T value) {
-    // ...
-  }
+    ~Vector(){
+        delete[] arr;
+    };
 
-  // Adds value to the end of the Vector.
-  // Complexity: O(1)
-  void add(T value) {
-    // ...
-  }
+    // Adds value at a specific index in the Vector.
+    // Complexity: O(n)
+    void insert(int index, T value) {
+        if(m_size == m_capacity){
+            T* tmpArr = new T[m_capacity*2];
+            memcpy(tmpArr, arr, (m_capacity)*sizeof(T) );
+            m_capacity *= 2;
+            delete[] arr;
+            arr = tmpArr;
+        }
+        for (int i = index; i <= m_size; i++){
+            T tmp = arr[i];
+            arr[i] = value;
+            value = tmp;
+        }
+        m_size++;
+    }
 
-  // Returns value at a specific index in the Vector
-  // Complexity: O(1)
-  T get(int index) {
-    // ...
-  }
+    // Adds value to the end of the Vector.
+    // Complexity: O(1)
+    void add(T value) {
+        if(m_size == m_capacity){
+            T* tmpArr = new T[m_capacity*2];
+            memcpy(tmpArr, arr, (m_capacity)*sizeof(T) );
+            m_capacity *= 2;
+            delete[] arr;
+            arr = tmpArr;
 
-  // Removes element at the specific index
-  // Complexity: O(n)
-  void remove(int index) {
-    // ...
-  }
+            arr[m_size] = value;
+            m_size++;
+        }else{
+            arr[m_size] = value;
+            m_size++;
+        }
+    }
 
-  // Removes element at the last index
-  // Complexity: O(1)
-  T pop() {
-    // ...
-  }
+    // For easier debugging
+    void getContent(){
+        for (int i = 0; i < m_size; i++) {
+            cout << get(i) << " ";
+        }
+        cout << endl;
+    }
 
-  // Returns the number of elements in the Vector.
-  // Complexity: O(1)
-  int size() {
-    // ...
-  }
+    // Returns value at a specific index in the Vector
+    // Complexity: O(1)
+    T get(int index) {
+        if(index < m_size){
+            return arr[index];
+        }else{
+            cout << "In function get(int index) index out of range" << endl;
+            return -1;
+        }
+    }
 
-  // Returns the total capacity of the Vector.
-  // Complexity: O(1)
-  int capacity() {
-    // ...
-  }
+    // Removes element at the specific index
+    // Complexity: O(n)
+    void remove(int index) {
+        if(index >= m_size){
+            cout << "In function remove(int index) index out of range" << endl;
+        }else{
+            if(m_size <= m_capacity/4){
+                T* tmpArr = new T[m_capacity/4];
+                memcpy(tmpArr, arr, index*sizeof(T));
+                memcpy(tmpArr+index, arr+index+1, (m_size-index-1)*sizeof(T));
+                m_capacity /= 4;
+                m_size--;
+                delete arr;
+                arr = tmpArr;
+            } else {
+                memmove(arr+index, arr+index+1, (m_size-index-1)*sizeof(T));
+                m_size--;
+            }
+        }
+    }
+
+
+    // Removes element at the last index
+    // Complexity: O(1)
+    T pop() {
+        m_size--;
+        return arr[m_size+1];
+    }
+
+    // Returns the number of elements in the Vector.
+    // Complexity: O(1)
+    int size() {
+        return m_size;
+    }
+
+    // Returns the total capacity of the Vector.
+    // Complexity: O(1)
+    int capacity() {
+        return m_capacity;
+    }
 };
+
+
+int main(){
+
+
+/*
+
+    Vector<int> v;
+    v.add(1);
+    v.getContent();
+    v.add(2);
+    v.getContent();
+    v.add(3);
+    v.getContent();
+    v.add(4);
+    v.getContent();
+    v.add(5);
+    v.getContent();
+
+
+    for(int i = 6; i < 18; i++){
+        v.add(i);
+    }
+    v.getContent();
+
+    for(int i = 0; i < 12; i++){
+        v.remove(2);
+        v.getContent();
+    }
+
+    v.insert(0,6);
+    v.getContent();
+    v.insert(1,7);
+    v.getContent();
+    v.insert(2,8);
+    v.getContent();
+    v.insert(0,9);
+    v.getContent();
+
+*/
+
+    return 0;
+}
--- week1/3-Stack/stack.cpp
+++ week1/3-Stack/stack.cpp
@@ -1,28 +1,190 @@
+#include <iostream>
+#include <typeinfo>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+/*
+#############################
+#########  QUEUE  ###########
+#############################
+*/
 template<class T>
-class Stack {
+class Queue {
+
+private:
+    struct Node{
+        T value;
+        Node* next;
+    };
+
+    int size_;
+    Node* head_;
+    Node* tail_;
+
 public:
 
-  // Adds value to the end of the Stack.
-  // Complexity: O(1)
-  void push(T value) {
-    // ...
-  }
+    Queue(){
+        size_ = 0;
+        head_ = NULL;
+        tail_ = NULL;
+    }
 
-  // Returns value from the end of the Stack and removes it.
-  // Complexity: O(n)
-  T pop() {
-    // ...
-  }
+    ~Queue(){
+        Node* tmp = head_;
+        while(tmp != NULL){
+            head_ = head_->next;
+            delete tmp;
+            tmp = head_;
+        }
+    }
 
-  // Returns value from the end of the Stack without removing it.
-  // Complexity: O(n)
-  T peek() {
-    // ...
-  }
+    // Adds value to the end of the Queue.
+    // Complexity: O(1)
+    void push(T value) {
+        if(size_ == 0){
+            head_ = new Node;
+            tail_ = head_;
+            tail_->next = NULL;
+            tail_->value = value;
+            size_++;
+        }else{
+            tail_->next = new Node;
+            tail_ = tail_->next;
+            tail_->next = NULL;
+            tail_->value = value;
+            size_++;
+        }
+    }
 
-  // Returns the number of elements in the Stack.
-  // Complexity: O(1)
-  int size() {
-    // ...
-  }
+    // Returns value from the front of the Queue and removes it.
+    // Complexity: O(1)
+    T pop() {
+        if(size_ > 0){
+            T toReturn = head_->value;
+            Node* tmp = head_;
+            head_ = head_->next;
+            delete tmp;
+            size_--;
+            return toReturn;
+        } else {
+            cout << "Cant pop, stack is empty ";
+            return -1;
+        }
+    }
+
+    // Returns value from the front of the Queue without removing it.
+    // Complexity: O(1)
+    T peek() {
+        if(size_ > 0){
+            return head_->value;
+        } else {
+            cout << "Cant peek, stack is empty ";
+            return -1;
+        }
+    }
+
+    // Returns the number of elements in the Queue.
+    // Complexity: O(1)
+    int size() {
+        return size_;
+    }
+
+    // Print the content
+    void DBG_getContent(){
+        Node* current = head_;
+        while(current != NULL){
+            cout << current->value << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
 };
+
+
+/*
+#############################
+#########  STACK  ###########
+#############################
+*/
+template<class T>
+class Stack {
+
+private:
+    Queue<T> q;
+
+public:
+
+    // Adds value to the end of the Stack.
+    // Complexity: O(1)
+    void push(T value) {
+        q.push(value);
+    }
+
+    // Returns value from the end of the Stack and removes it.
+    // Complexity: O(n)
+    T pop() {
+        if(q.size()){
+            for(int i = 0; i < q.size()-1; i++){
+                q.push(q.pop());
+            }
+            return q.pop();
+        } else {
+            cout << "Cant pop queue is empty" << endl;
+            return -1;
+        }
+    }
+
+    // Returns value from the end of the Stack without removing it.
+    // Complexity: O(n)
+    T peek() {
+        if(q.size()){
+            for(int i = 0; i < q.size()-1; i++){
+                q.push(q.pop());
+            }
+            T toReturn = q.peek();
+            q.push(q.pop());
+            return toReturn;
+        } else {
+            cout << "Cant peek queue is empty" << endl;
+            return -1;
+        }
+    }
+
+    // Returns the number of elements in the Stack.
+    // Complexity: O(1)
+    int size() {
+        return q.size();
+    }
+};
+
+int main(){
+
+    Stack<int> st;
+
+    st.push(1);
+    cout << st.peek() << endl;
+    st.push(2);
+    cout << st.peek() << endl;
+    st.push(3);
+    cout << st.peek() << endl;
+    st.push(4);
+    cout << st.peek() << endl;
+    cout << endl;
+    cout << st.pop() << endl;
+    cout << st.peek() << endl;
+    while(st.size()){
+        cout << st.pop() << " ";
+    }
+    cout << endl;
+    cout << st.pop() << endl;
+    cout << st.peek() << endl;
+
+    return 0;
+}
+
+
+
