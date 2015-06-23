07855a2cd1261128600cc84b26168c3bf5ac2f3b
samir-github <Samir>
2015-06-24 00:05:58 +0300

    Revert "Homework"
    This reverts commit 86290de09cb206b84a7d35c3bab27606a87bcea5.

--- week3/1-BST/bst.cpp
+++ week3/1-BST/bst.cpp
@@ -1,84 +1,15 @@
-#include <iostream>
-#include <typeinfo>
-#include <climits>
-#include <limits>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-
-using namespace std;
-
 class Node {
 public:
-
-    Node(int value, Node* left, Node* right):
-        value(value), left(left), right(right){
-    }
-    int value;
-    Node* left;
-    Node* right;
+  int value;
+  Node* left;
+  Node* right;
 };
 
 class BST {
 public:
 
-    // Checks if a binary tree is a binary search tree.
-    bool isBST(Node* root) {
-
-        if (root == NULL){
-            return true;
-        }
-
-        if(root->left != NULL){
-            if(root->left->value > root->value){
-                return false;
-            }
-        }
-        if(root->right != NULL){
-            if(root->right->value < root->value){
-                return false;
-            }
-        }
-
-        return (isBST(root->left) && isBST(root->right));
-
-    }
+  // Checks if a binary tree is a binary search tree.
+  bool isBST(Node* root) {
+    // ...
+  }
 };
-
-int main(){
-
-/*    // DEPTH:  0        1        2        3
-    Node* node = new Node(10,
-                          new Node(5,
-                                   new Node(3,
-                                            NULL,
-                                            NULL),
-                                   new Node(7,
-                                            NULL,
-                                            NULL)),
-                          new Node(15,
-                                   NULL,
-                                   new Node(20,
-                                            NULL,
-                                            NULL)));
-
-    BST bst;
-    cout << bst.isBST(node);
-*/
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
