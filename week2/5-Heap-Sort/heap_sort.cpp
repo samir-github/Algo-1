e34ca57f39937f1a70af8bc8c7a2d354eaa1666f
samir-github <Samir>
2015-06-23 13:00:59 +0300

    Homework

--- week2/5-Heap-Sort/heap_sort.cpp
+++ week2/5-Heap-Sort/heap_sort.cpp
@@ -1,8 +1,99 @@
+#include <iostream>
+#include <typeinfo>
+#include <climits>
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+
 class HeapSort {
+
 public:
 
-  // Sorts a sequence of integers.
-  void sort(int* sequence, int size) {
-    // ...
-  }
+    // Sorts a sequence of integers.
+    void sort(int* sequence, int size) {
+
+        make_heap(sequence, size);
+
+        while(size--){
+
+            swap(sequence[0],sequence[size]);
+
+            int parent = 0;
+
+            while(parent*2+2 < size && (sequence[parent] < sequence[parent*2+1] || sequence[parent] < sequence[parent*2+2])){
+
+                int bigger_child = sequence[parent*2+1] > sequence[parent*2+2] ? parent*2+1 : parent*2+2;
+
+                swap(sequence[bigger_child], sequence[parent]);
+                parent = bigger_child;
+            }
+            if(parent*2+1 < size && sequence[parent] < sequence[parent*2+1]){
+                swap(sequence[parent], sequence[parent*2+1]);
+            }
+        }
+    }
+
+private:
+    // bigger elements are higher
+    void make_heap(int* seq, int size){
+
+        for(int i = size/2; i >= 0; i--){
+
+            int parent = i;
+
+            while(parent*2+2 < size && (seq[parent] < seq[parent*2+1] || seq[parent] < seq[parent*2+2])){
+
+                int bigger_child = seq[parent*2+1] > seq[parent*2+2] ? parent*2+1 : parent*2+2;
+
+                swap(seq[bigger_child], seq[parent]);
+                parent = bigger_child;
+            }
+            if(parent*2+1 < size && seq[parent] < seq[parent*2+1]){
+                swap(seq[parent], seq[parent*2+1]);
+            }
+        }
+    }
+
 };
+
+
+int main(){
+    /*
+    int arr[] = {10,3,-5,8,2,5,-12,3,13,4,0,0,20,-20,INT_MAX,INT_MIN,140,360,420,720,1337};
+
+    HeapSort hs;
+    hs.sort(arr, sizeof(arr)/ sizeof(int));
+
+    for(int i : arr){
+        cout << i << " ";
+    }
+    */
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- week2/6-K-Lists/k_lists.cpp
+++ week2/6-K-Lists/k_lists.cpp
@@ -1,18 +1,145 @@
+#include <iostream>
+#include <typeinfo>
+#include <climits>
+#include <limits>
+#include <algorithm>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
 class Node {
 public:
-  int value;
-  Node* next;
+
+    Node(int value = INT_MIN, Node* next = NULL): value(value), next(next){
+    }
+    int value;
+    Node* next;
 };
+
+
 
 class KLists {
 public:
 
-  // Merge K sorted lists.
-  Node merge(vector<Node> lists) {
-    // ...
-  }
+    // Merge K sorted lists.
+    Node merge(vector<Node> lists) {
+
+        Node answer;
+        Node* answer_tail = &answer;
+
+        make_heap(lists, lists.size());
+
+        while(!lists.empty()){
+
+            answer_tail->next = new Node(lists[0].value, lists[0].next);
+            answer_tail = answer_tail->next;
+            cout << answer_tail->value << endl;
+
+            if(lists[0].next != NULL){
+
+                lists[0] = *(lists[0].next);
+                heapify(lists, lists.size());
+
+            } else {
+
+                lists[0] = lists.back();
+                lists.pop_back();
+                heapify(lists, lists.size());
+            }
+        }
+
+
+
+
+
+        for(auto e : lists){
+            cout << e.value << " ";
+        }
+
+    }
+
+private:
+    //min heap
+    void make_heap(vector<Node> &seq, int size){
+
+        for(int i = size/2; i >= 0; i--){
+
+            int parent = i;
+
+            while(parent*2+2 < size && (seq[parent].value > seq[parent*2+1].value || seq[parent].value > seq[parent*2+2].value)){
+
+                int smallest_child = seq[parent*2+1].value < seq[parent*2+2].value ? parent*2+1 : parent*2+2;
+
+                swap(seq[smallest_child], seq[parent]);
+                parent = smallest_child;
+            }
+            if(parent*2+1 < size && seq[parent].value > seq[parent*2+1].value){
+                swap(seq[parent], seq[parent*2+1]);
+            }
+        }
+    }
+    void heapify(vector<Node> &seq, int size){
+
+        int parent = 0;
+
+            while(parent*2+2 < size && (seq[parent].value > seq[parent*2+1].value || seq[parent].value > seq[parent*2+2].value)){
+
+                int smallest_child = seq[parent*2+1].value < seq[parent*2+2].value ? parent*2+1 : parent*2+2;
+
+                swap(seq[smallest_child], seq[parent]);
+                parent = smallest_child;
+            }
+            if(parent*2+1 < size && seq[parent].value > seq[parent*2+1].value){
+                swap(seq[parent], seq[parent*2+1]);
+            }
+    }
+
 };
+
+
+int main(){
+
+
+/*
+    vector<Node> input = {
+        Node(3, new Node(5, new Node(7, new Node(9, NULL)))),
+        Node(2, new Node(4, new Node(6, NULL))),
+        Node(0, new Node(1, new Node(8, new Node(10, NULL))))
+    };
+
+    KLists klist;
+    klist.merge(input);
+*/
+
+
+/*
+    for(auto i : input){
+        while(i.next != NULL){
+            cout << i.value << " ";
+            i = *i.next;
+        }
+        cout << i.value << endl;
+    }
+*/
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
