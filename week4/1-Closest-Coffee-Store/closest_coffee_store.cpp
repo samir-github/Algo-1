d48d90084c9a77a6fafd516a475875c9484c00d4
samir-github <Samir>
2015-06-30 14:48:36 +0300

    homework

--- week4/1-Closest-Coffee-Store/closest_coffee_store.cpp
+++ week4/1-Closest-Coffee-Store/closest_coffee_store.cpp
@@ -1,8 +1,94 @@
+#include <iostream>
+#include <typeinfo>
+#include <climits>
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <cstring>
+
+#define sz 6
+using namespace std;
+
+
+
 class ClosestCoffeeStore {
 public:
 
-  // Finds the closest coffee store to a point.
-  int closestCoffeeStore(bool** graph, bool* isCoffeStore, int size, int startingPoint) {
-    // ...
-  }
+    // Finds the closest coffee store to a point.
+    int closestCoffeeStore(bool** graph, bool* isCoffeStore, int size, int startingPoint) {
+
+        bool* visited = new bool[size];
+        memset(visited, 0, sizeof(bool)*size);
+
+        queue<int> q;
+        q.push(startingPoint);
+
+        while(!q.empty()){
+            if(isCoffeStore[q.front()]){
+                return q.front();
+            }
+            for(int i = 0; i < size; i++){
+                if (graph[q.front()][i]){
+                    q.push(i);
+                }
+            }
+            q.pop();
+        }
+
+    return -1;
+    }
 };
+
+/*
+
+int main(){
+
+
+    bool** arr_p_p = new bool*[sz]{
+                        new bool[sz] {0, 1, 0, 1, 0, 0},
+                        new bool[sz] {1, 0, 1, 0, 0, 0},
+                        new bool[sz] {0, 1, 0, 0, 1, 0},
+                        new bool[sz] {1, 0, 0, 0, 0, 0},
+                        new bool[sz] {0, 0, 1, 0, 0, 1},
+                        new bool[sz] {0, 0, 0, 0, 1, 0} };
+
+    bool* is_coffe_shop = new bool[sz] {0, 0, 1, 0, 0, 1};
+
+
+    ClosestCoffeeStore closed_coffe_shop;
+
+    cout << closed_coffe_shop.closestCoffeeStore(arr_p_p, is_coffe_shop, sz, 0);
+
+
+
+
+
+    return 0;
+}
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
