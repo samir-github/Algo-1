8de616656bb00938a2540b2be978d481aee13fc0
samir-github <Samir>
2015-07-01 15:01:18 +0300

    Syncing

--- week4/2-Valid-Directories/valid_directories.cpp
+++ week4/2-Valid-Directories/valid_directories.cpp
@@ -1,7 +1,65 @@
+#include <iostream>
+#include <typeinfo>
+#include <climits>
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <cstring>
+
+using namespace std;
+
+#define sz 6
+
 class ValidDirectories {
+
 public:
 
-  bool isValid(int** graph, int size) {
-    // ...
-  }
+    bool isValid(int** graph, int size) {
+        bool is
+    }
 };
+
+
+int main(){
+
+
+   int** arr_p_p = new int*[sz]{ new int[sz] {0, 1, 0, 1, 0, 2},
+                                 new int[sz] {0, 0, 2, 0, 0, 0},
+                                 new int[sz] {0, 0, 0, 0, 0, 0},
+                                 new int[sz] {1, 0, 0, 0, 2, 0},
+                                 new int[sz] {0, 0, 0, 0, 0, 0},
+                                 new int[sz] {0, 0, 0, 0, 0, 0} };
+
+    ValidDirectories vd;
+    vd.isValid(arr_p_p,sz);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
